1f4747b78f03d523388d0737f582de36a6591575
kishorvrs <kishorvrs@gmail.com>
2020-11-24 09:51:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/linearprogressdialog/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.linearprogressdialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog dialog;
+    Handler handler;
+    Runnable runnable;
+    Timer timer;
+    int i=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dialog=new ProgressDialog(MainActivity.this);
+        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        dialog.setIndeterminate(false);
+        dialog.setTitle("Progress Dialog");
+        dialog.setMessage("please wait...");
+        dialog.show();
+        dialog.setProgress(0);
+        dialog.setMax(100);
+        handler = new Handler();
+        runnable=new Runnable() {
+            @Override
+            public void run() {
+                i=i+5;
+                if (i<=100){
+                    dialog.setProgress(i);
+                }else{
+                    timer.cancel();
+                    dialog.cancel();
+                    i=0;
+                }
+
+            }
+        };
+        timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        },8000,500);
+    }
+}
